98d3b418932c40de9930ab40612b8228aa5e43b6
Mandeep Singh <techiesoulsin@gmail.com>
2020-01-18 01:40:12 +0800

    improved invitation tab

--- app/src/main/java/com/cenesbeta/adapter/InvitationListItemAdapter.java
+++ app/src/main/java/com/cenesbeta/adapter/InvitationListItemAdapter.java
@@ -1,31 +1,50 @@
 package com.cenesbeta.adapter;
 
 import android.os.Build;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.cenesbeta.R;
+import com.cenesbeta.activity.CenesBaseActivity;
 import com.cenesbeta.bo.Event;
+import com.cenesbeta.bo.EventMember;
 import com.cenesbeta.dto.HomeScreenDto;
 import com.cenesbeta.fragment.dashboard.HomeFragment;
+import com.cenesbeta.fragment.dashboard.HomeFragmentV2;
+import com.cenesbeta.util.CenesUtils;
+import com.cenesbeta.util.RoundedImageView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class InvitationListItemAdapter extends BaseExpandableListAdapter {
 
-    private HomeFragment homeFragment;
+    private HomeFragmentV2 homeFragmentV2;
     private HomeScreenDto homeScreenDto;
     private LayoutInflater inflter;
 
 
-    public InvitationListItemAdapter(HomeFragment homeFragment, HomeScreenDto homeScreenDto) {
+    public InvitationListItemAdapter(HomeFragmentV2 homeFragmentV2, HomeScreenDto homeScreenDto) {
 
-        this.homeFragment = homeFragment;
+        this.homeFragmentV2 = homeFragmentV2;
         this.homeScreenDto = homeScreenDto;
-        inflter = (LayoutInflater.from(homeFragment.getContext()));
+        inflter = (LayoutInflater.from(homeFragmentV2.getContext()));
 
     }
 
@@ -86,7 +105,122 @@ public class InvitationListItemAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+
+        EventViewHolder eventViewHolder;
+        if(convertView == null) {
+            eventViewHolder = new EventViewHolder();
+            convertView = inflter.inflate(R.layout.adapter_invitation_list_item, null);
+
+            eventViewHolder.tvEventTitle = (TextView) convertView.findViewById(R.id.tv_event_title);
+            eventViewHolder.tvEventLocation = (TextView) convertView.findViewById(R.id.tv_event_location);
+            eventViewHolder.tvEventDate = (TextView) convertView.findViewById(R.id.tv_event_date);
+            eventViewHolder.tvHostName = (TextView) convertView.findViewById(R.id.tv_host_name);
+            eventViewHolder.tvNonCenesCount = (TextView)convertView.findViewById(R.id.tv_non_cenes_count);
+            eventViewHolder.llEventLocationSection = (LinearLayout) convertView.findViewById(R.id.ll_event_location_section);
+            eventViewHolder.recyclerViewGuests = (RecyclerView) convertView.findViewById(R.id.recycler_view_guests);
+            eventViewHolder.rvHostImage = (RoundedImageView) convertView.findViewById(R.id.rv_host_image);
+            eventViewHolder.rlNonCenesCountView = (RelativeLayout) convertView.findViewById(R.id.rl_non_cenes_count_view);
+
+            convertView.setTag(eventViewHolder);
+
+        } else {
+            eventViewHolder = (EventViewHolder)convertView.getTag();
+        }
+
+        Event event = getChild(groupPosition, childPosition);
+        List<EventMember> eventMembers = event.getEventMembers();
+
+        eventViewHolder.tvEventTitle.setText(event.getTitle());
+        if (!CenesUtils.isEmpty(event.getLocation())) {
+            eventViewHolder.llEventLocationSection.setVisibility(View.GONE);
+            eventViewHolder.tvEventLocation.setText(event.getLocation());
+        } else {
+            eventViewHolder.llEventLocationSection.setVisibility(View.VISIBLE);
+        }
+
+        String eventDate = CenesUtils.hmmaa.format(new Date(event.getStartTime()))+"-"+CenesUtils.hmmaa.format(new Date(event.getEndTime()));
+        eventViewHolder.tvEventDate.setText(eventDate.toUpperCase());
+
+        //Lets find Event Host
+        EventMember eventHost = null;
+        for (EventMember eventMember: eventMembers) {
+            if (eventMember.getUserId() != null && eventMember.getUserId().equals(event.getCreatedById())) {
+                eventHost = eventMember;
+                break;
+            }
+        }
+        if (eventHost != null) {
+
+            if (eventHost.getUser() != null && !CenesUtils.isEmpty(eventHost.getUser().getPicture())) {
+                RequestOptions requestOptions = new RequestOptions();
+                requestOptions.placeholder(R.drawable.profile_pic_no_image);
+                requestOptions.circleCrop();
+                Glide.with(homeFragmentV2.getContext()).load(eventHost.getUser().getPicture()).apply(requestOptions).into(eventViewHolder.rvHostImage);
+            } else {
+                eventViewHolder.rvHostImage.setImageResource(R.drawable.profile_pic_no_image);
+            }
+            eventViewHolder.tvHostName.setText(eventHost.getUser().getName());
+        } else {
+            eventViewHolder.rvHostImage.setImageResource(R.drawable.profile_pic_no_image);
+        }
+
+
+        //Lets show Event Members without images
+        List<EventMember> eventMembersWithoutHostAsCenesMember = new ArrayList<>();
+        for (EventMember eventMember: eventMembers) {
+            if (eventMember.getUserId() != null && eventMember.getUserId().equals(event.getCreatedById())) {
+                continue;
+            }
+            //Lets exclude who are not cenes member
+            if (eventMember.getUserId() == null) {
+                continue;
+            }
+            eventMembersWithoutHostAsCenesMember.add(eventMember);
+        }
+
+        int loopLimit = 0;
+        if (eventMembersWithoutHostAsCenesMember.size() > 0) {
+            eventViewHolder.recyclerViewGuests.setVisibility(View.VISIBLE);
+            if (eventMembersWithoutHostAsCenesMember.size() > 3) {
+                loopLimit = 2;
+            } else {
+                loopLimit = eventMembersWithoutHostAsCenesMember.size() - 1;
+            }
+
+            List<EventMember> eventMembersListForRecycler = new ArrayList<>();
+            int lootIndex = 0;
+            for (final EventMember eveMemWithoutHost: eventMembersWithoutHostAsCenesMember) {
+                if (lootIndex > loopLimit) {
+                    break;
+                }
+                eventMembersListForRecycler.add(eveMemWithoutHost);
+            }
+            InvitationTabItemRecyclerAdapter invitationTabItemRecyclerAdapter = new InvitationTabItemRecyclerAdapter(homeFragmentV2, eventMembersListForRecycler);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(homeFragmentV2.getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+            eventViewHolder.recyclerViewGuests.setLayoutManager(mLayoutManager);
+            eventViewHolder.recyclerViewGuests.setItemAnimator(new DefaultItemAnimator());
+            eventViewHolder.recyclerViewGuests.setAdapter(invitationTabItemRecyclerAdapter);
+
+        } else {
+            eventViewHolder.recyclerViewGuests.setVisibility(View.GONE);
+        }
+
+        //Lets show Event Members without images
+        List<EventMember> eventMembersAsNonCenesMember = new ArrayList<>();
+        for (EventMember eventMember: eventMembers) {
+            //Lets find who are not cenes member
+            if (eventMember.getUserId() == null) {
+                eventMembersAsNonCenesMember.add(eventMember);
+            }
+        }
+        if (eventMembersAsNonCenesMember.size() > 0) {
+            eventViewHolder.rlNonCenesCountView.setVisibility(View.VISIBLE);
+            eventViewHolder.tvNonCenesCount.setText("+"+eventMembersAsNonCenesMember.size());
+        } else {
+            eventViewHolder.rlNonCenesCountView.setVisibility(View.GONE);
+        }
+
+        return convertView;
     }
 
     @Override
@@ -94,6 +228,13 @@ public class InvitationListItemAdapter extends BaseExpandableListAdapter {
         return false;
     }
 
+    class EventViewHolder {
+        private TextView tvEventTitle, tvEventLocation, tvEventDate, tvHostName, tvNonCenesCount;
+        private LinearLayout llEventLocationSection;
+        private RelativeLayout rlNonCenesCountView;
+        private RoundedImageView rvHostImage;
+        private RecyclerView recyclerViewGuests;
+    }
     class HeaderViewHolder {
         private TextView tvHeader;
     }
--- app/src/main/java/com/cenesbeta/fragment/dashboard/HomeFragmentV2.java
+++ app/src/main/java/com/cenesbeta/fragment/dashboard/HomeFragmentV2.java
@@ -18,6 +18,7 @@ import com.cenesbeta.Manager.InternetManager;
 import com.cenesbeta.R;
 import com.cenesbeta.activity.CenesBaseActivity;
 import com.cenesbeta.adapter.CalendarTabExpandableListAdapter;
+import com.cenesbeta.adapter.InvitationListItemAdapter;
 import com.cenesbeta.api.HomeScreenAPI;
 import com.cenesbeta.bo.Event;
 import com.cenesbeta.bo.User;
@@ -36,11 +37,8 @@ import com.google.gson.reflect.TypeToken;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -54,6 +52,7 @@ public class HomeFragmentV2 extends CenesFragment {
     private LinearLayout llCalendarDateBar;
     private MaterialCalendarView mcvHomeCalendar;
     private ExpandableListView elvHomeListView;
+    private ExpandableListView elvInvitationListView;
 
 
     private HomeScreenDto homeScreenDto;
@@ -62,6 +61,7 @@ public class HomeFragmentV2 extends CenesFragment {
     private UserManager userManager;
     public  User loggedInUser;
     private CalendarTabExpandableListAdapter calendarTabExpandableListAdapter;
+    private InvitationListItemAdapter invitationListItemAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -79,6 +79,7 @@ public class HomeFragmentV2 extends CenesFragment {
         llCalendarDateBar = (LinearLayout) view.findViewById(R.id.ll_calendar_date_bar);
         mcvHomeCalendar = (MaterialCalendarView) view.findViewById(R.id.mcv_home_calendar);
         elvHomeListView = (ExpandableListView) view.findViewById(R.id.elv_home_list_view);
+        elvInvitationListView = (ExpandableListView)view.findViewById(R.id.elv_invitation_list_view);
 
         //Click Listeners
         tvCalendarTab.setOnClickListener(onClickListener);
@@ -131,14 +132,19 @@ public class HomeFragmentV2 extends CenesFragment {
 
     public void calendarTabPressed() {
 
+        homeScreenDto.setTabSelected(HomeScreenDto.HomeTabs.Calendar);
         tvCalendarTab.setBackground(getResources().getDrawable(R.drawable.xml_border_bottom_black));
         tvInvitationTab.setBackground(null);
+        processCalendarTabData(homeScreenDto.getHomeEvents());
     }
 
     public void invitationTabPressed() {
 
+        homeScreenDto.setTabSelected(HomeScreenDto.HomeTabs.Invitation);
         tvInvitationTab.setBackground(getResources().getDrawable(R.drawable.xml_border_bottom_black));
         tvCalendarTab.setBackground(null);
+        processCalendarTabData(homeScreenDto.getAcceptedEvents());
+
     }
 
     public void plusButtonPressed() {
@@ -165,16 +171,38 @@ public class HomeFragmentV2 extends CenesFragment {
 
     public void loadCalendarTabData(){
 
-        homeScreenDto.setHomeScreenAPICall(HomeScreenDto.HomeScreenAPICall.Home);
         AsyncTaskDto asyncTaskDto = new AsyncTaskDto();
         asyncTaskDto.setApiUrl(UrlManagerImpl.prodAPIUrl + HomeScreenAPI.get_homescreen_events);
         asyncTaskDto.setQueryStr("user_id=" + loggedInUser.getUserId() + "&date="+new Date().getTime());
         asyncTaskDto.setAuthToken(loggedInUser.getAuthToken());
 
-        getAsyncDto(asyncTaskDto);
+        getAsyncDto(asyncTaskDto, HomeScreenDto.HomeScreenAPICall.Home);
+
+        loadInvitationTabData();
     }
 
-    public void getAsyncDto(AsyncTaskDto asyncTaskDto){
+    public void loadInvitationTabData() {
+
+        AsyncTaskDto acceptedAsyncTaskDto = new AsyncTaskDto();
+        acceptedAsyncTaskDto.setApiUrl(UrlManagerImpl.prodAPIUrl + HomeScreenAPI.get_gathering_evnets);
+        acceptedAsyncTaskDto.setQueryStr("userId=" + loggedInUser.getUserId() + "&status=Going&timestamp="+new Date().getTime()+"&pageNumber=0&offSet=20");
+        acceptedAsyncTaskDto.setAuthToken(loggedInUser.getAuthToken());
+        getAsyncDto(acceptedAsyncTaskDto, HomeScreenDto.HomeScreenAPICall.Accepted);
+
+        AsyncTaskDto penidngAsyncTaskDto = new AsyncTaskDto();
+        penidngAsyncTaskDto.setApiUrl(UrlManagerImpl.prodAPIUrl + HomeScreenAPI.get_gathering_evnets);
+        penidngAsyncTaskDto.setQueryStr("userId=" + loggedInUser.getUserId() + "&status=Pending&timestamp="+new Date().getTime()+"&pageNumber=0&offSet=20");
+        penidngAsyncTaskDto.setAuthToken(loggedInUser.getAuthToken());
+        getAsyncDto(penidngAsyncTaskDto, HomeScreenDto.HomeScreenAPICall.Pending);
+
+        AsyncTaskDto declinedAsyncTaskDto = new AsyncTaskDto();
+        declinedAsyncTaskDto.setApiUrl(UrlManagerImpl.prodAPIUrl + HomeScreenAPI.get_gathering_evnets);
+        declinedAsyncTaskDto.setQueryStr("userId=" + loggedInUser.getUserId() + "&status=NotGoing&timestamp="+new Date().getTime()+"&pageNumber=0&offSet=20");
+        declinedAsyncTaskDto.setAuthToken(loggedInUser.getAuthToken());
+        getAsyncDto(declinedAsyncTaskDto, HomeScreenDto.HomeScreenAPICall.Declined);
+    }
+
+    public void getAsyncDto(AsyncTaskDto asyncTaskDto, final HomeScreenDto.HomeScreenAPICall homeScreenAPICall){
 
        new ProfileAsyncTask.CommonGetRequestTask(new ProfileAsyncTask.CommonGetRequestTask.AsyncResponse() {
            @Override
@@ -183,11 +211,25 @@ public class HomeFragmentV2 extends CenesFragment {
                 try {
                     boolean success = response.getBoolean("success");
 
-                    if ( success ) {
+                    if (success) {
                         Gson gson = new GsonBuilder().create();
                         Type listType = new TypeToken<List<Event>>(){}.getType();
                         List<Event> events = gson.fromJson(response.getJSONArray("data").toString(), listType);
-                        processCalendarTabData(events);
+
+                        if (homeScreenAPICall.equals(HomeScreenDto.HomeScreenAPICall.Home)) {
+                            homeScreenDto.setHomeEvents(events);
+                            processCalendarTabData(events);
+
+                        } else if (homeScreenAPICall.equals(HomeScreenDto.HomeScreenAPICall.Accepted)) {
+                            homeScreenDto.setAcceptedEvents(events);
+
+                        } else if (homeScreenAPICall.equals(HomeScreenDto.HomeScreenAPICall.Pending)) {
+                            homeScreenDto.setPendingEvents(events);
+
+                        } else if (homeScreenAPICall.equals(HomeScreenDto.HomeScreenAPICall.Declined)) {
+                            homeScreenDto.setDeclinedEvents(events);
+
+                        }
 
                     }
 
@@ -205,26 +247,26 @@ public class HomeFragmentV2 extends CenesFragment {
         List<Long> headersTimstamp = new ArrayList<>();
 
         Map<String,List<Event>> mapListEvent = new HashMap<>();
+        if (events != null) {
+            for (Event event : events) {
+                try{
 
+                    String headerTitle = CenesUtils.EEEMMMMdd.format(new Date(event.getStartTime()));
 
-        for (Event event : events) {
-            try{
+                    if( mapListEvent.containsKey(headerTitle) ){
+                        List<Event> eventList = mapListEvent.get(headerTitle);
+                        eventList.add(event);
+                        mapListEvent.put(headerTitle,eventList);
+                    } else {
+                        List<Event> eventList = new ArrayList<>();
+                        eventList.add(event);
+                        mapListEvent.put(headerTitle,eventList);
+                        headersTimstamp.add(event.getStartTime());
 
-                String headerTitle = CenesUtils.EEEMMMMdd.format(new Date(event.getStartTime()));
-
-                if( mapListEvent.containsKey(headerTitle) ){
-                    List<Event> eventList = mapListEvent.get(headerTitle);
-                    eventList.add(event);
-                    mapListEvent.put(headerTitle,eventList);
-                } else {
-                    List<Event> eventList = new ArrayList<>();
-                    eventList.add(event);
-                    mapListEvent.put(headerTitle,eventList);
-                    headersTimstamp.add(event.getStartTime());
-
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
                 }
-            } catch (Exception e){
-                e.printStackTrace();
             }
         }
         Collections.sort(headersTimstamp);
@@ -235,8 +277,16 @@ public class HomeFragmentV2 extends CenesFragment {
         homeScreenDto.setHomeDataHeaders(headers);
         homeScreenDto.setHomeDataListMap(mapListEvent);
 
-        calendarTabExpandableListAdapter = new CalendarTabExpandableListAdapter(this, homeScreenDto);
-        elvHomeListView.setAdapter(calendarTabExpandableListAdapter);
+        if (homeScreenDto.getTabSelected().equals(HomeScreenDto.HomeTabs.Calendar)) {
+            elvHomeListView.setVisibility(View.VISIBLE);
+            elvInvitationListView.setVisibility(View.GONE);
+            calendarTabExpandableListAdapter = new CalendarTabExpandableListAdapter(this, homeScreenDto);
+            elvHomeListView.setAdapter(calendarTabExpandableListAdapter);
+        } else {
+            elvHomeListView.setVisibility(View.GONE);
+            elvInvitationListView.setVisibility(View.VISIBLE);
+            invitationListItemAdapter = new InvitationListItemAdapter(this, homeScreenDto);
+            elvInvitationListView.setAdapter(invitationListItemAdapter);
+        }
     }
-
 }
